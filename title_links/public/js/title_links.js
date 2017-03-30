b7ea42c97209c2a3b6e2934f1d176985937b000b
Giovanni <giovanni@codrotech.com>
2017-03-31 00:33:14 +0200

    - fix links in list_view for version 8.

--- title_links/public/js/title_links.js
+++ title_links/public/js/title_links.js
@@ -360,7 +360,7 @@ frappe.ui.form.GridRow = frappe.ui.form.GridRow.extend({
 (function () {
 	frappe.templates["list_item_main"] = frappe.templates["list_item_main"].replace(
 		'<a class="filterable h6 text-muted grey" data-filter="{%= col.fieldname %},=,{%= value %}">{%= value %}</a>',
-		'<a class="filterable h6 text-muted grey" data-filter="{%= col.fieldname %},=,{%= value %}">{%= frappe.format(value, col.df, null, data) %}</a>'
+		'{%= frappe.format(value, col.df, null, data) %}'
 	);
 	delete frappe.template.compiled["list_item_main"];
 })();
