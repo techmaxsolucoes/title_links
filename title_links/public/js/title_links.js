11c3b85eba6d5f81a53b2683576b06d59dfddba8
joezsweet <joezsweet@tiscali.it>
2017-04-03 12:45:07 +0200

    Update title_links.js

--- title_links/public/js/title_links.js
+++ title_links/public/js/title_links.js
@@ -359,8 +359,8 @@ frappe.ui.form.GridRow = frappe.ui.form.GridRow.extend({
 
 (function () {
 	frappe.templates["list_item_main"] = frappe.templates["list_item_main"].replace(
-		'<a class="filterable h6 text-muted grey" data-filter="{%= col.fieldname %},=,{%= value %}">{%= value %}</a>',
-		'{%= frappe.format(value, col.df, null, data) %}'
+		'<a class="filterable text-muted grey ellipsis" data-filter="{%= col.fieldname %},=,{%= value %}">{%= value %}</a>,
+		'<a class="filterable text-muted grey ellipsis" data-filter="{%= col.fieldname %},=,{%= value %}">{%= frappe.format(value, col.df, null, data) %}</a>'
 	);
 	delete frappe.template.compiled["list_item_main"];
 })();
